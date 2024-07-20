3e4906863263183405b89fee7d38198cd5f263f5
Valerii Yaremenko <v.yaremenko@andersenlab.com>
2024-07-20 03:20:49 +0200

    Deleted Auth test endpoints Added Test controller

---
+++ src/Autodissmark.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Autodissmark.API.Responses.BaseResponses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Autodissmark.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet("test")]
+    public async Task<IActionResult> Test()
+    {
+        return Ok(new SuccessResponse<string>("It works! Everything is alright!!!"));
+    }
+}
